0355d40c47d6ff47de4b42e5036d28b64ad951b8
Charel Felten <charel.felten@gmail.com>
2022-09-21 13:07:44 +0200

    integrating nextjs by following tutorial

---
+++ docs/nextjs-blog/README.md
@@ -0,0 +1,6 @@
+This is a starter template for [Learn Next.js](https://nextjs.org/learn).
+
+
+Notes:
+
+- use <Link> tag to move between pages faster than with default html css
---
+++ docs/nextjs-blog/pages/_app.js
@@ -0,0 +1,13 @@
+// used to load global CSS files
+// Any styles imported in _app.js will be applied globally, to all pages of the application.
+
+import '../styles/global.css';
+
+
+export default function App({ Component, pageProps }) {
+    return <Component {...pageProps} />;
+}
+
+//   This App component is the top-level component which will be common across all the different pages. You can use this App component to keep state when navigating between pages, for example.
+
+// In Next.js, you can add global CSS files by importing them from pages/_app.js. You cannot import global CSS anywhere else.
